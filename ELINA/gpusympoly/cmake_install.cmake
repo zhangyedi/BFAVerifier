95268522eeae4194a9f0590032b4c62757630e85
winlere <winlere@qq.com>
2024-07-17 05:11:56 -0600

    feat: rename to gpusympoly

--- ELINA/gpusympoly/cmake_install.cmake
+++ ELINA/gpusympoly/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/winlere/BFAVerifier/ELINA/gpupoly
+# Install script for directory: /home/winlere/BFAVerifier/ELINA/gpusympoly
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/winlere/BFAVerifier/ELINA/gpupoly/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/winlere/BFAVerifier/ELINA/gpusympoly/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
